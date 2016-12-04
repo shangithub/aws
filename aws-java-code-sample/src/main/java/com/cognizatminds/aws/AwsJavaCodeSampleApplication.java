e592e5040980119c6635629fb845d44861b8fb4c
SHAN <SHAN@SHAN-PC>
2016-12-04 15:50:49 -0500

    AWS sample

---
+++ aws-java-code-sample/src/main/java/com/cognizatminds/aws/AwsJavaCodeSampleApplication.java
@@ -0,0 +1,12 @@
+package com.cognizatminds.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJavaCodeSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsJavaCodeSampleApplication.class, args);
+	}
+}
